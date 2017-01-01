063e13f08e867d0eb1a1fc63129d8f3b515b35c2
Huijian Jiang <waynejiang@hotmail.com>
2017-01-01 20:40:13 +0800

    Add class sample

---
+++ PyExamples/classPractice.py
@@ -0,0 +1,11 @@
+Structure and Behavior
+* Classes define the structure and behavior of objects
+* An object's class controls its initialization
+
+Classes are a Tool
+* Classes make complex problems tractable
+* Classes can make simple solutions overly complex
+
+Initialization
+* __init__() is an initializer, not a constrctor
+* self is similar to this in C++ or Java
