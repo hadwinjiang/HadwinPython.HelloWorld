38a893fac70ff1c79c18dfa90ac79dcbdd281183
hjjaws <hjjaws@amazon.com>
2020-02-08 11:31:37 +0800

    add new item view

--- globomantics/app.py
+++ globomantics/app.py
@@ -8,4 +8,9 @@ def home():
     return render_template("home.html")
     # return "home"
 
+
+@app.route("/item/new")
+def new_item():
+    return render_template("new_item.html")
+
 # 127.0.0.1:5000
