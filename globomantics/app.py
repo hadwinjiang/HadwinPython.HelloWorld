5b9a4c9f84040db7b652794dcdd846f635b0fc62
hjjaws <hjjaws@amazon.com>
2020-02-08 11:21:42 +0800

    static content and placeholder

--- globomantics/app.py
+++ globomantics/app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return "home"
+    return render_template("home.html")
+    # return "home"
 
 # 127.0.0.1:5000
