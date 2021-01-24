031764619dc49cb75007f74d3cc490932e3ebbe0
henryjiang@mac <henryjiang@mac>
2021-01-24 22:13:17 +0800

    use iso6346

--- CorePython/classandoo/mod01shipping/shipping.py
+++ CorePython/classandoo/mod01shipping/shipping.py
@@ -1,3 +1,6 @@
+import classandoo.mod01shipping.iso6346 as iso6346
+
+
 class ShippingContainer:
     next_serial = 1337  # Class attribute
 
@@ -6,6 +9,13 @@ class ShippingContainer:
         result = cls.next_serial
         cls.next_serial += 1
         return result
+
+    @staticmethod
+    def _make_bic_code(owner_code, serial):
+        return iso6346.create(
+            owner_code=owner_code,
+            serial=str(serial).zfill(6)
+        )
 
     @classmethod
     def create_empty(cls, owner_code):
@@ -18,7 +28,10 @@ class ShippingContainer:
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code  # Instance attribute
         self.contents = contents
-        self.serial = ShippingContainer._generate_serial()
+        self.bic = ShippingContainer._make_bic_code(
+            owner_code,
+            serial=ShippingContainer._generate_serial()
+        )
 
 # from classandoo.mod01shipping.shipping import ShippingContainer
 # c2 = ShippingContainer("YML", ["books"])
@@ -26,6 +39,7 @@ class ShippingContainer:
 # c3 = ShippingContainer("XUS", ["computers"])
 # c7 = ShippingContainer.create_empty("YML")
 # c8 = ShippingContainer.create_with_items("MAE", {"food", "textiles", "minerals"})
+# c = ShippingContainer.create_empty("YML")
 
 # Scopes in Python  -   LEGB
 # Local -   Inside the current function
