a3d08656982a5f41e4666f12fd372d2b0480e768
henryjiang@mac <henryjiang@mac>
2021-01-24 21:43:10 +0800

    static method

--- CorePython/classandoo/mod01shipping/shipping.py
+++ CorePython/classandoo/mod01shipping/shipping.py
@@ -1,15 +1,21 @@
 class ShippingContainer:
     next_serial = 1337  # Class attribute
 
+    @staticmethod
+    def _generate_serial():
+        result = ShippingContainer.next_serial
+        ShippingContainer.next_serial += 1
+        return result
+
     def __init__(self, owner_code, contents):
         self.owner_code = owner_code  # Instance attribute
         self.contents = contents
-        self.serial = ShippingContainer.next_serial
-        ShippingContainer.next_serial += 1
+        self.serial = ShippingContainer._generate_serial()
 
 # from classandoo.mod01shipping.shipping import ShippingContainer
 # c2 = ShippingContainer("YML", ["books"])
 # c2.owner_code
+# c3 = ShippingContainer("XUS", ["computers"])
 
 # Scopes in Python  -   LEGB
 # Local -   Inside the current function
